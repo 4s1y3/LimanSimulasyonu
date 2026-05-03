af8239f128a5a4674158af564f7985c4c606a0a8
idalozyurtt-png <idalozyurtt@gmail.com>
2026-05-03 15:35:42 +0300

    Update MyStack.java
    yeni

--- src/dataStructures/MyStack.java
+++ src/dataStructures/MyStack.java
@@ -1,81 +1,73 @@
-package dataStructures;
+package datastructures;
+
 
 public class MyStack<T> {
-
     private Node<T> top;
     private int size;
     private int capacity;
+   
 
     public MyStack(int capacity) {
-        this.capacity = capacity;
-        this.size = 0;
         this.top = null;
+        this.size = 0;
+        this.capacity = capacity;
     }
 
-  
     public boolean push(T data) {
-
+        
         if (isFull()) {
+           
             return false;
-        }
-
+        } 
         Node<T> newNode = new Node<>(data);
-
         newNode.next = top;
         top = newNode;
-
         size++;
         return true;
     }
 
-    
     public T pop() {
-
-        if (isEmpty()) {
+        if (isEmpty()){ 
             return null;
         }
-
-        T temp = top.data;
+      //veri farklı
+      T data = top.data;
         top = top.next;
-
         size--;
-
-        return temp;
+        return data;
     }
 
-    
     public T peek() {
-
-        if (isEmpty()) {
+        
+       if (isEmpty()) {
             return null;
         }
-
-        return top.data;
+      return top.data;
     }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public boolean isFull() {
+    
+     public boolean isFull() {
         return size == capacity;
+    }
+    
+    public boolean isEmpty() {
+        return top == null;
     }
 
     public int size() {
         return size;
     }
-
+    
     public int getCapacity() {
         return capacity;
     }
-}
-    public int getSize() {
-        return size;
+    
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
 
-    public int getCapacity() {
-        return capacity;
+
+    public void clear() {
+        top = null;
+        size = 0;
     }
 }
-
-
