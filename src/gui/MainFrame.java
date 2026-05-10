16d42cc13ef8df4014d91c0c5682452e4b032316
idalozyurtt-png <idalozyurtt@gmail.com>
2026-05-10 10:41:52 +0300

    Update MainFrame.java

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,4 +1,17 @@
 package gui;
 
+import logic.LimanYonetici;
+import gui.AnaPencere;
+import javax.swing.SwingUtilities;
+
 public class MainFrame {
+    public static void main(String[] args) {
+        LimanYonetici yonetici = new LimanYonetici();
+        yonetici.yukle();
+        
+        SwingUtilities.invokeLater(() -> {
+            new AnaPencere(yonetici).setVisible(true);
+        });
+    }
 }
+
