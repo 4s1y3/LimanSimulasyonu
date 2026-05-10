1122832747dbd97fe7cdfcf4c5f283b42c44701b
idalozyurtt-png <idalozyurtt@gmail.com>
2026-05-10 10:54:47 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+
+import logic.LimanYonetici;
+import gui.AnaPencere;
+import javax.swing.SwingUtilities;
+
 public class Main {
-}
+    public static void main(String[] args) {
+        LimanYonetici yonetici = new LimanYonetici();
+        yonetici.yukle();
+        
+        SwingUtilities.invokeLater(() -> {
+            new AnaPencere(yonetici).setVisible(true);
+        });
